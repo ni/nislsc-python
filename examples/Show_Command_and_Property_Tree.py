687d8fb56cfc33d81be7aab606c9f63060e75f51
Ang, Qi Yuen <qiyuen.ang@emerson.com>
2025-07-07 17:09:53 +0800

    Fix linters

--- examples/Show_Command_and_Property_Tree.py
+++ examples/Show_Command_and_Property_Tree.py
@@ -1,21 +1,26 @@
+"""Demonstrates how to query and display the command and property tree.
+
+It connects to the specified SLSC device, retrieves its available
+commands, properties, and physical channels, and organizes this
+information into a structured JSON format. The module supports querying
+both device-level and physical channel-level commands and properties.
+"""
+
 import json
 import sys
+
 from nislsc._nislsc import NISLSC
-from nislsc.constants import ReservationAccess, PropertyAccess, DataType, TableScaleCoercion
+from nislsc.constants import (
+    DataType,
+    PropertyAccess,
+    ReservationAccess,
+)
 
-def show_command_and_property_tree():
-    """
-    Show the command and property tree of the NISLSC library.
-    """
 
+def show_command_and_property_tree(device_names: str) -> dict:
+    """Show the command and property tree of the NISLSC library."""
     with NISLSC() as nislsc:
         with nislsc.initialize_library() as lib:
-            
-            # Initialize session
-            if len(sys.argv) > 1:
-                device_names = sys.argv[1]
-            else:
-                print("Please provide device names.")
 
             connection_timeout = 10.0
             reservation_access = 1
@@ -25,160 +30,181 @@ def show_command_and_property_tree():
             data = {
                 "name": device_names,
                 "Commands": [],
-                "Properties": [],                
+                "Properties": [],
             }
 
             with lib.initialize_session_with_devices(
-                device_names, connection_timeout, reservation_access, reservation_group, reservation_timeout
+                device_names,
+                connection_timeout,
+                reservation_access,
+                reservation_group,
+                reservation_timeout,
             ) as session:
 
-                device_commands = session.get_device_property_string_array(device_names, "Dev.Commands")
+                device_commands = session.get_device_property_string_array(
+                    device_names, "Dev.Commands"
+                )
 
-                device_properties = session.get_device_property_string_array(device_names, "Dev.Properties")
+                device_properties = session.get_device_property_string_array(
+                    device_names, "Dev.Properties"
+                )
 
-                device_physical_channels = session.get_device_property_string_array(device_names, "Dev.PhysChans")
+                device_physical_channels = session.get_device_property_string_array(
+                    device_names, "Dev.PhysChans"
+                )
 
                 for physical_channel in device_physical_channels:
-                    data[f"{physical_channel}"] = {
-                        "Commands": [],
-                        "Properties": []
-                    }
-                
+                    data[f"{physical_channel}"] = {"Commands": [], "Properties": []}
+
                 for device_command in device_commands:
                     with session.open_device_command(device_names, device_command) as command:
                         name = device_command
                         description = command.get_command_property_string("Cmd.Descr")
                         access = ReservationAccess(command.get_command_property_int32("Cmd.Access"))
-                        data['Commands'].append({
-                            "name": name,
-                            "description": description,
-                            "access": access.name
-                        })
-                        
+                        data["Commands"].append(
+                            {"name": name, "description": description, "access": access.name}
+                        )
+
                         print(data)
 
                 for device_property in device_properties:
                     with session.open_device_property(device_names, device_property) as property:
                         name = device_property
                         datatype = DataType(property.get_property_property_int32("Prop.DataType"))
-                        
-                        if datatype.value == 1:
-                            current_value = session.get_generic_property_bool(device_names, device_property)
-                        elif datatype.value == 2:
-                            current_value = session.get_generic_property_double(device_names, device_property)
-                        elif datatype.value == 3:
-                            current_value = session.get_generic_property_int32(device_names, device_property)
-                        elif datatype.value == 4:
-                            current_value = session.get_generic_property_int64(device_names, device_property)
-                        elif datatype.value == 5:
-                            current_value = session.get_generic_property_string(device_names, device_property)
-                        elif datatype.value == 6:
-                            current_value = session.get_generic_property_uint32(device_names, device_property)
-                        elif datatype.value == 7:
-                            current_value = session.get_generic_property_uint64(device_names, device_property)
-                        elif datatype.value == 8:
-                            current_value = session.get_generic_property_bool_array(device_names, device_property)
-                        elif datatype.value == 9:
-                            current_value = session.get_generic_property_double_array(device_names, device_property)
-                        elif datatype.value == 10:
-                            current_value = session.get_generic_property_int32_array(device_names, device_property)
-                        elif datatype.value == 11:
-                            current_value = session.get_generic_property_int64_array(device_names, device_property)
-                        elif datatype.value == 12:
-                            current_value = session.get_generic_property_string_array(device_names, device_property)
-                        elif datatype.value == 13:
-                            current_value = session.get_generic_property_uint32_array(device_names, device_property)
-                        elif datatype.value == 14:
-                            current_value = session.get_generic_property_uint64_array(device_names, device_property)
-                        else:
-                            current_value = "Unsupported DataType"
+
+                        current_value = get_property(
+                            session, device_names, device_property, datatype
+                        )
 
                         access = PropertyAccess(property.get_property_property_int32("Prop.Access"))
                         description = property.get_property_property_string("Prop.Descr")
 
                         min_value = property.get_property_property_string("Prop.MinValue")
                         max_value = property.get_property_property_string("Prop.MaxValue")
-                        
-                        range_value = f"[{min_value}, {max_value}]" if min_value is not None and max_value is not None else ""
-                        data['Properties'].append({
-                            "name": name,
-                            "current_value": current_value,
-                            "datatype": datatype.name,
-                            "range": range_value,
-                            "access": access.name,
-                            "description": description
-                        })
-                        
-                for physical_channel in device_physical_channels:
 
-                    physical_channel_commands = session.get_physical_channel_property_string_array(physical_channel, "PhysChan.Commands")
-                    physical_channel_properties = session.get_physical_channel_property_string_array(physical_channel, "PhysChan.Properties")
-
-                    for physical_channel_command in physical_channel_commands:
-                        with session.open_physical_channel_command(physical_channel, physical_channel_command) as command:
-                            name = physical_channel_command
-                            description = command.get_command_property_string("Cmd.Descr")
-                            access = ReservationAccess(command.get_command_property_int32("Cmd.Access"))
-                            data[physical_channel]["Commands"].append({
-                                "name": name,
-                                "description": description,
-                                "access": access.name
-                            })
-
-                    for physical_channel_property in physical_channel_properties:
-                        with session.open_physical_channel_property(physical_channel, physical_channel_property) as property:
-                            name = physical_channel_property
-                            datatype = DataType(property.get_property_property_int32("Prop.DataType"))
-                            
-                            if datatype.value == 1:
-                                current_value = session.get_generic_property_bool(physical_channel, physical_channel_property)
-                            elif datatype.value == 2:
-                                current_value = session.get_generic_property_double(physical_channel, physical_channel_property)
-                            elif datatype.value == 3:
-                                current_value = session.get_generic_property_int32(physical_channel, physical_channel_property)
-                            elif datatype.value == 4:
-                                current_value = session.get_generic_property_int64(physical_channel, physical_channel_property)
-                            elif datatype.value == 5:
-                                current_value = session.get_generic_property_string(physical_channel, physical_channel_property)
-                            elif datatype.value == 6:
-                                current_value = session.get_generic_property_uint32(physical_channel, physical_channel_property)
-                            elif datatype.value == 7:
-                                current_value = session.get_generic_property_uint64(physical_channel, physical_channel_property)
-                            elif datatype.value == 8:
-                                current_value = session.get_generic_property_bool_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 9:
-                                current_value = session.get_generic_property_double_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 10:
-                                current_value = session.get_generic_property_int32_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 11:
-                                current_value = session.get_generic_property_int64_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 12:
-                                current_value = session.get_generic_property_string_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 13:
-                                current_value = session.get_generic_property_uint32_array(physical_channel, physical_channel_property)
-                            elif datatype.value == 14:
-                                current_value = session.get_generic_property_uint64_array(physical_channel, physical_channel_property)
-                            else:
-                                current_value = "Unsupported DataType"
-
-                            access = PropertyAccess(property.get_property_property_int32("Prop.Access"))
-                            description = property.get_property_property_string("Prop.Descr")
-
-                            min_value = property.get_property_property_string("Prop.MinValue")
-                            max_value = property.get_property_property_string("Prop.MaxValue")
-
-                            range_value = f"[{min_value}, {max_value}]" if min_value and max_value else ""
-                            data[physical_channel]["Properties"].append({
+                        range_value = (
+                            f"[{min_value}, {max_value}]"
+                            if min_value is not None and max_value is not None
+                            else ""
+                        )
+                        data["Properties"].append(
+                            {
                                 "name": name,
                                 "current_value": current_value,
                                 "datatype": datatype.name,
                                 "range": range_value,
                                 "access": access.name,
-                                "description": description
-                            })
+                                "description": description,
+                            }
+                        )
+
+                for physical_channel in device_physical_channels:
+
+                    physical_channel_commands = session.get_physical_channel_property_string_array(
+                        physical_channel, "PhysChan.Commands"
+                    )
+                    physical_channel_properties = (
+                        session.get_physical_channel_property_string_array(
+                            physical_channel, "PhysChan.Properties"
+                        )
+                    )
+
+                    for physical_channel_command in physical_channel_commands:
+                        with session.open_physical_channel_command(
+                            physical_channel, physical_channel_command
+                        ) as command:
+                            name = physical_channel_command
+                            description = command.get_command_property_string("Cmd.Descr")
+                            access = ReservationAccess(
+                                command.get_command_property_int32("Cmd.Access")
+                            )
+                            data[physical_channel]["Commands"].append(
+                                {"name": name, "description": description, "access": access.name}
+                            )
+
+                    for physical_channel_property in physical_channel_properties:
+                        with session.open_physical_channel_property(
+                            physical_channel, physical_channel_property
+                        ) as property:
+                            name = physical_channel_property
+                            datatype = DataType(
+                                property.get_property_property_int32("Prop.DataType")
+                            )
+
+                            current_value = get_property(
+                                session, physical_channel, physical_channel_property, datatype
+                            )
+
+                            access = PropertyAccess(
+                                property.get_property_property_int32("Prop.Access")
+                            )
+                            description = property.get_property_property_string("Prop.Descr")
+
+                            min_value = property.get_property_property_string("Prop.MinValue")
+                            max_value = property.get_property_property_string("Prop.MaxValue")
+
+                            range_value = (
+                                f"[{min_value}, {max_value}]" if min_value and max_value else ""
+                            )
+                            data[physical_channel]["Properties"].append(
+                                {
+                                    "name": name,
+                                    "current_value": current_value,
+                                    "datatype": datatype.name,
+                                    "range": range_value,
+                                    "access": access.name,
+                                    "description": description,
+                                }
+                            )
 
                 complete_data = json.dumps(data, indent=4)
                 print(complete_data)
 
 
-show_command_and_property_tree()
+def get_property(session, resource, property, datatype):
+    """Retrieve the current value of a property based on its data type."""
+    if datatype.value == DataType.BOOL:
+        current_value = session.get_generic_property_bool(resource, property)
+    elif datatype.value == DataType.DOUBLE:
+        current_value = session.get_generic_property_double(resource, property)
+    elif datatype.value == DataType.INT32:
+        current_value = session.get_generic_property_int32(resource, property)
+    elif datatype.value == DataType.INT64:
+        current_value = session.get_generic_property_int64(resource, property)
+    elif datatype.value == DataType.STRING:
+        current_value = session.get_generic_property_string(resource, property)
+    elif datatype.value == DataType.UINT32:
+        current_value = session.get_generic_property_uint32(resource, property)
+    elif datatype.value == DataType.UINT64:
+        current_value = session.get_generic_property_uint64(resource, property)
+    elif datatype.value == DataType.BOOL_ARRAY:
+        current_value = session.get_generic_property_bool_array(resource, property)
+    elif datatype.value == DataType.DOUBLE_ARRAY:
+        current_value = session.get_generic_property_double_array(resource, property)
+    elif datatype.value == DataType.INT32_ARRAY:
+        current_value = session.get_generic_property_int32_array(resource, property)
+    elif datatype.value == DataType.INT64_ARRAY:
+        current_value = session.get_generic_property_int64_array(resource, property)
+    elif datatype.value == DataType.STRING_ARRAY:
+        current_value = session.get_generic_property_string_array(resource, property)
+    elif datatype.value == DataType.UINT32_ARRAY:
+        current_value = session.get_generic_property_uint32_array(resource, property)
+    elif datatype.value == DataType.UINT64_ARRAY:
+        current_value = session.get_generic_property_uint64_array(resource, property)
+    else:
+        current_value = "Unsupported DataType"
+
+    return current_value
+
+
+def main():
+    """Main function to execute the command."""
+    if len(sys.argv) > 1:
+        device_names = sys.argv[1]
+    else:
+        print("Please provide device names.")
+
+    show_command_and_property_tree(device_names)
+
+
+main()
